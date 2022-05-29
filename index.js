f9171283fb557f8a704658270f8e459c5fb64492
leandro <leandrochaparro19@gmail.com>
2022-05-29 03:16:05 -0300

    Juego batalla

---
+++ index.js
@@ -0,0 +1,196 @@
+function Inicio(){
+
+console.log("██████╗  █████╗ ████████╗ █████╗ ██╗     ██╗      █████╗     ██████╗  ██████╗ ██╗  ██╗███████╗███╗   ███╗ ██████╗ ███╗   ██╗");
+console.log("██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██║     ██║     ██╔══██╗    ██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝████╗ ████║██╔═══██╗████╗  ██║");
+console.log("██████╔╝███████║   ██║   ███████║██║     ██║     ███████║    ██████╔╝██║   ██║█████╔╝ █████╗  ██╔████╔██║██║   ██║██╔██╗ ██║");
+console.log("██╔══██╗██╔══██║   ██║   ██╔══██║██║     ██║     ██╔══██║    ██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██║╚██╔╝██║██║   ██║██║╚██╗██║");
+console.log("██████╔╝██║  ██║   ██║   ██║  ██║███████╗███████╗██║  ██║    ██║     ╚██████╔╝██║  ██╗███████╗██║ ╚═╝ ██║╚██████╔╝██║ ╚████║");
+console.log("╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝    ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝");
+console.log("                                                                                                                            ");
+console.log("%c                                        ⠀⠀⠀⠀⠀⢸⠓⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⠀⠀⢸⠀⠀⠑⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠙⢤⡷⣤⣦⣀⠤⠖⠚⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⣠⡿⠢⢄⡀⠀⡇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⠸⠷⣶⠂⠀⠀⠀⣀⣀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⢸⣃⠀⠀⠉⠳⣷⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⢉⡭⠋",'color: #ff00ff');
+console.log("%c                                        ⠀⠘⣆⠀⠀⠀⠁⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠋⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠘⣦⠆⠀⠀⢀⡎⢹⡀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⡀⣠⠔⠋⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⡏⠀⠀⣆⠘⣄⠸⢧⠀⠀⠀⠀⢀⣠⠖⢻⠀⠀⠀⣿⢥⣄⣀⣀⣀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⢸⠁⠀⠀⡏⢣⣌⠙⠚⠀⠀⠠⣖⡛⠀⣠⠏⠀⠀⠀⠇⠀⠀⠀⠀⢙⣣⠄",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⢸⡀⠀⠀⠳⡞⠈⢻⠶⠤⣄⣀⣈⣉⣉⣡⡔⠀⠀⢀⠀⠀⣀⡤⠖⠚⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⡼⣇⠀⠀⠀⠙⠦⣞⡀⠀⢀⡏⠀⢸⣣⠞⠀⠀⠀⡼⠚⠋⠁⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⢰⡇⠙⠀⠀⠀⠀⠀⠀⠉⠙⠚⠒⠚⠉⠀⠀⠀⠀⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⢧⡀⠀⢠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⠙⣶⣶⣿⠢⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⠀⠀⠉⠀⠀⠀⠙⢿⣳⠞⠳⡄⠀⠀⠀⢀⡞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+console.log("%c                                        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠀⠀⠹⣄⣀⡤⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",'color: #ff00ff');
+}
+
+function perdio(){
+console.log("%c                                          ██████╗ ███████╗██████╗ ██████╗  ██████╗ ████████╗ █████╗     ██╗","color:#ff6347");
+console.log("%c                                          ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝██╔══██╗    ██║","color:#ff6347");
+console.log("%c                                          ██║  ██║█████╗  ██████╔╝██████╔╝██║   ██║   ██║   ███████║    ██║","color:#ff6347");
+console.log("%c                                          ██║  ██║██╔══╝  ██╔══██╗██╔══██╗██║   ██║   ██║   ██╔══██║    ╚═╝","color:#ff6347");
+console.log("%c                                          ██████╔╝███████╗██║  ██║██║  ██║╚██████╔╝   ██║   ██║  ██║    ██╗","color:#ff6347");
+console.log("%c                                          ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝    ╚═╝","color:#ff6347");                                                                                                                                                         
+}
+
+function gano(){
+console.log("%c                                         ██╗   ██╗██╗ ██████╗████████╗ ██████╗ ██████╗ ██╗ █████╗     ██╗","color:#008000");
+console.log("%c                                         ██║   ██║██║██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██║██╔══██╗    ██║","color:#008000");
+console.log("%c                                         ██║   ██║██║██║        ██║   ██║   ██║██████╔╝██║███████║    ██║","color:#008000");
+console.log("%c                                         ╚██╗ ██╔╝██║██║        ██║   ██║   ██║██╔══██╗██║██╔══██║    ╚═╝","color:#008000");
+console.log("%c                                          ╚████╔╝ ██║╚██████╗   ██║   ╚██████╔╝██║  ██║██║██║  ██║    ██╗","color:#008000");
+console.log("%c                                           ╚═══╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝    ╚═╝","color:#008000");
+                                                                                                                                                             
+}
+
+function comienzo(){
+    console.clear();
+    console.log(" ██████╗ ██████╗ ███╗   ███╗██╗███████╗███╗   ██╗███████╗ █████╗     ██╗      █████╗     ██████╗  █████╗ ████████╗ █████╗ ██╗     ██╗      █████╗ ");
+    console.log("██╔════╝██╔═══██╗████╗ ████║██║██╔════╝████╗  ██║╚══███╔╝██╔══██╗    ██║     ██╔══██╗    ██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██║     ██║     ██╔══██╗");
+    console.log("██║     ██║   ██║██╔████╔██║██║█████╗  ██╔██╗ ██║  ███╔╝ ███████║    ██║     ███████║    ██████╔╝███████║   ██║   ███████║██║     ██║     ███████║");
+    console.log("██║     ██║   ██║██║╚██╔╝██║██║██╔══╝  ██║╚██╗██║ ███╔╝  ██╔══██║    ██║     ██╔══██║    ██╔══██╗██╔══██║   ██║   ██╔══██║██║     ██║     ██╔══██║");
+    console.log("╚██████╗╚██████╔╝██║ ╚═╝ ██║██║███████╗██║ ╚████║███████╗██║  ██║    ███████╗██║  ██║    ██████╔╝██║  ██║   ██║   ██║  ██║███████╗███████╗██║  ██║");
+    console.log(" ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝    ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝"); 
+                                                                                                                                                    
+}
+
+class Pokemon{
+    constructor(nombre,vida,ataque,defensa,nivel){
+    this.nombre=nombre;
+    this.vida=vida;
+    this.ataque=ataque;
+    this.defensa=defensa;
+    this.nivel=nivel;
+    }
+    mostrarInfo(){
+        return `
+        Pokemon:${this.nombre}
+        Vida:${this.vida}
+        Ataque:${this.ataque}
+        Defensa:${this.defensa}
+        Nivel:${this.nivel}`;
+    }
+    set SetDaño(valor){
+        if(this.defensa.length>0){
+        if(this.defensa.length>valor.length){
+        this.defensa = this.defensa.substring(0,this.defensa.length-valor.length);
+        }else{
+        this.defensa = this.defensa.substring(0,this.defensa.length-valor.length);
+        this.vida =this.vida.substring(0,this.vida.length-(valor.length-this.defensa.length));
+        }
+        }
+        else{
+        this.vida =this.vida.substring(0,this.vida.length-valor.length);
+        }
+    }
+    set SetEvloucion(aux){
+        this.vida=this.vida.concat(aux);
+        this.defensa=this.defensa.concat(aux);
+        this.ataque=this.ataque.concat("█████");
+        this.nivel++;
+    }
+    get GetVida(){
+        return this.vida;
+    }
+    Muere(){
+        if(this.vida.length<=0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+function seleccionPokemon(pokemon1,dato){
+let vida="████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████";
+switch(dato){
+    case "1":
+    case 1:
+        pokemon1 = new Pokemon("Pikachu",vida,"███████████","██████████████████████████████████████████",1);
+        return pokemon1;
+    case "2":
+    case 2:
+        pokemon1 = new Pokemon("Charizar",vida,"██████","██████████████████████████████████████████████ ",1);
+        return pokemon1;
+    case "3":
+    case 3:
+        pokemon1 = new Pokemon("Charmander",vida,"███████","██████████████████████████████████",1);
+        return pokemon1;
+    case "4":
+    case 4:
+        pokemon1 = new Pokemon("Bulbasaur",vida,"████████","██████████████████████████████████████████",1);
+        return pokemon1;
+    case "5":
+    case 5:
+        pokemon1 = new Pokemon("Squirtle",vida,"████████","██████████████████████████████████████████",1);
+        return pokemon1;
+    case "6":
+    case 6:
+        pokemon1 = new Pokemon("Snorlax",vida,"███████","██████████████████████████████████████████",1);
+        return pokemon1;
+}}
+
+Inicio();
+let poke;
+do{
+poke = prompt("Selecciona tu pokemon \n 1:Pikachu \n 2:Charizar \n 3:Charmander \n 4:Bulbasaur \n 5:Squirtle \n 6:Snorlax");
+}while(poke != "1" && poke !="2" && poke!="3" && poke != "4" && poke !="5" && poke!="6");
+
+let pc;
+let pokemonUsers = new Pokemon;
+let pokemonPc = new Pokemon;
+pokemonUsers = seleccionPokemon(pokemonUsers,poke);
+while(true){
+    pc = Math.floor(Math.random() * (7 - 1)) + 1;
+    if(pc != poke){
+    pokemonPc = seleccionPokemon(pokemonPc,pc);   
+    break;
+    }
+}
+
+console.log("USUARIO: "+pokemonUsers.mostrarInfo()+"\nPC: "+pokemonPc.mostrarInfo());
+
+let Ronda=1;
+let terminar =false;
+let evolucionUser,evolucionPc;
+comienzo()
+
+ while(true){   
+    if(!pokemonUsers.Muere() && !pokemonPc.Muere()){
+    if(Ronda>5){
+       evolucionUser=Math.floor(Math.random() * (20 - 1)) + 1;
+       evolucionPc= Math.floor(Math.random() * (20 - 1)) + 1;
+       if(evolucionPc==15){
+           console.log(`${pokemonPc.nombre} Evoluciono!`);
+           pokemonPc.SetEvloucion = "███████████████████████";
+       }
+       if(evolucionUser==15){
+           console.log(`${pokemonUsers.nombre} Evoluciono!`);
+           pokemonUsers.SetEvloucion ="███████████████████████";
+       }
+    }
+    console.log(`--------------------------------------------------------------------RONDA ${Ronda}--------------------------------------------------------------------`);    
+    pokemonPc.SetDaño=pokemonUsers.ataque;
+    pokemonUsers.SetDaño=pokemonPc.ataque;
+    console.log(`%c ${pokemonUsers.nombre} Nv ${pokemonUsers.nivel}: `,`color:green`);
+    console.log(`%c ${pokemonUsers.ataque}`,`color:#ffa500`);
+    console.log(`%c ${pokemonUsers.GetVida}`,`color:green`);
+    console.log(`%c ${pokemonUsers.defensa}`,`color:#48d1cc`);
+    console.log(`%c ${pokemonPc.nombre} Nv ${pokemonPc.nivel}: `,`color:red`);
+    console.log(`%c ${pokemonPc.ataque}`,`color:#ffa500`);
+    console.log(`%c ${pokemonPc.GetVida}`,`color:red`);
+    console.log(`%c ${pokemonPc.defensa}`,`color:#48d1cc`);
+    }else{
+        terminar=true;
+        if(pokemonUsers.Muere()==true){
+            console.log(`---------------------------------------------------------------Ganador: ${pokemonPc.nombre}---------------------------------------------------------------`);
+            perdio();
+        }else{
+            console.log(`---------------------------------------------------------------Ganador: ${pokemonUsers.nombre}---------------------------------------------------------------`);
+            gano();
+        }
+        break;
+    }
+    Ronda++;
+}
